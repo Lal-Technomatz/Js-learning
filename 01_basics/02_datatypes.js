9980a74995e3b133d534b82021be10d052b1c6f8
Lal Singh Bhadoria <98153394+Lal-Technomatz@users.noreply.github.com>
2024-01-05 12:49:03 +0000

    Added Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use_strict"; //treat all js code as newer version
+
+// alert( 3 + 3 ) //we are using node js not browser.
+
+console.log( 3 
+    + 
+    3 ) //code readability should be high
+
+console.log("Lal Singh")
+
+// for documentation we can follow tc39.es and Mdn
+
+
+let name = "Lal Singh"
+let age = 30
+let isLoggedIn = false
+
+// Number 2 to power 53
+// bigint 
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined
+// symbol = unique
+
+
+// object
+
+console.log(typeof "Lal Singh")
+console.log(typeof age)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
