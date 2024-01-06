e2e93092046001a7736a6a98fbf0cc8aaa268c56
Lal Singh Bhadoria <98153394+Lal-Technomatz@users.noreply.github.com>
2024-01-06 11:23:32 +0000

    Conversion and Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,5 +30,50 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// *****************Operations*****************
+
+let value = 3
+
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Lal Singh"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2) => 12
+// console.log(1 + "2") => 12
+// console.log("1" + 2 + 2) => 122
+// console.log(1 + 2 + "2") => 32
+
+// console.log(+true); => 1
+// console.log(+""); => 0
+
+let num1, num2, num3
+
+num1 =num2 =num3 = 2 + 2
+
+let gameCounter = 100
+console.log(gameCounter);  // 100
+
+
+++gameCounter;
+
+console.log(gameCounter);  // 101
+console.log(++gameCounter); // 102
+console.log(gameCounter++);  // 102
+console.log(gameCounter);  // 103
+
