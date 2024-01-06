04d8cc1757f46ecdf281c7f0bd0d626cb86f7827
Lal Singh Bhadoria <98153394+Lal-Technomatz@users.noreply.github.com>
2024-01-06 09:09:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "String" => NaN
+// true => 1, false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = ""
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanLoggedIn);
+// console.log(typeof booleanLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "abs" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
