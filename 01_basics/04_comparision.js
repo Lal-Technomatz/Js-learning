46f7d8f1183a537b313ab37674463f5b2c753e4d
Lal Singh Bhadoria <98153394+Lal-Technomatz@users.noreply.github.com>
2024-01-08 06:04:36 +0000

    Comparision learning

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);   // true
+// console.log("02" > 1);  // true
+
+// this code must be avoid  because it create confusion so avoid this type comparision.
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === this is a strict check because it check the datatypes also.
+
+console.log("2" == 2); // it return true
+console.log("2" === 2);// it return false
