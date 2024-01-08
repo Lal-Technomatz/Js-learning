83d482200587e196a337ee463b83045e35bb2828
Lal Singh Bhadoria <98153394+Lal-Technomatz@users.noreply.github.com>
2024-01-08 12:22:11 +0000

    date in js

---
+++ 01_basics/07_dateinjs.js
@@ -0,0 +1,33 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+// let myCreatedDate = new Date("2023-01-23");
+let myCreatedDate = new Date("01-23-2023");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000)); // to convert it in to second devide by 1000 and to avoid decimal value use Math function.
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay())
+
+// console.log(newDate.toLocaleString('default', {
+//     weekday: "short"
+// }));
