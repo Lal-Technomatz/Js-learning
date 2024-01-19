9c9fd971b67e2d12c5221baef5b2663e129a89fb
Lal Singh Bhadoria <98153394+Lal-Technomatz@users.noreply.github.com>
2024-01-19 12:42:20 +0000

    functions basics

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,7 +28,7 @@ function addTwoNumbers(number1, number2) {
 const result = addTwoNumbers(3, 5);
 // console.log(result);
 
-function loginUserMessage(username){
+function loginUserMessage(username = "lal"){     // default value of username
     if(!username){  // if(username === undefined){
         console.log("Please Enter a User Name");
         return
@@ -37,5 +37,36 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("Hitesh"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
+// function calculateCartPrice(...num1){      // here '...' is called as rest operator 
+function calculateCartPrice(val1, val2, ...num1){      // here '...' is called as rest operator 
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+const myNewArray = [100, 200, 300, 400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 5000]));
